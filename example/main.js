b1044aeb24df4467d26c36af4973b50380a44674
Abdullah Mara <zull.mara00@gmail.com>
2017-07-04 17:53:15 +0300

    :tada:

---
+++ example/main.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+
+const components = [
+  {
+    name: 'blabla', // tmp name
+    template: '<div>BlaBla</div>'
+  }
+];
+
+const leylim = new Leylim({
+  el: '#app',
+  components
+});
---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import babel from 'rollup-plugin-babel';
+import babelrc from 'babelrc-rollup';
+import istanbul from 'rollup-plugin-istanbul';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+let pkg = require('./package.json');
+let external = Object.keys(pkg.dependencies);
+
+let plugins = [
+  babel(babelrc()),
+  serve({
+    open: true,
+    host: 'localhost',
+    contentBase: ['./'],
+    port: 1992
+  }),
+  livereload({
+    watch: ['dist', 'example']
+  })
+];
+
+if (process.env.BUILD !== 'production') {
+  plugins.push(
+    istanbul({
+      exclude: ['test/**/*', 'node_modules/**/*']
+    })
+  );
+}
+
+export default {
+  entry: 'lib/index.js',
+  plugins: plugins,
+  external: external,
+  targets: [
+    {
+      dest: pkg.main,
+      format: 'umd',
+      moduleName: 'Leylim',
+      sourceMap: true
+    }
+  ]
+};
