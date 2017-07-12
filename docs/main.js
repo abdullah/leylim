23924b5d1d3e33d0b846f61410d1eed80eede98b
Abdullah Mara <zull.mara00@gmail.com>
2017-07-12 11:38:36 +0300

    Improvement jsdoc

--- docs/main.js
+++ docs/main.js
@@ -17,6 +17,7 @@ const rowList = [
     template: `<div contenteditable>une kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren </div>`
   }
 ]
+
 const leylim = new Leylim({
   el: '#app',
   components,
