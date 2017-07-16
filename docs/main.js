98755baad3044c5670f25a1e49725a291a08cddf
Abdullah Mara <zull.mara00@gmail.com>
2017-07-16 22:13:06 +0300

    implementation custom style

--- docs/main.js
+++ docs/main.js
@@ -1,4 +1,6 @@
 /* eslint-disable */
+let leylim;
+
 window.onload = () => {
 
   let components = [
@@ -7,19 +9,20 @@ window.onload = () => {
       template: '<div contenteditable>Lorem ipsum dolor sit <b>amet</b>, consectetur adipisicing elit. At reiciendis et aliquid dolores eligendi repellendus, voluptate ut odio omnis, <img src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg" /> aliquam deserunt reprehenderit eum exercitationem neque nemo veritatis eveniet molestiae!</div>'
     },
     {
-      name: 'fooooo', // tmp name
-      template: '<div contenteditable><i>Lorem</i> ipsum dolor sit amet.</div>'
+      name: 'fooo', // tmp name
+      template: `<div class="fooo" contenteditable><i>Lorem</i> ipsum dolor sit amet.</div>`,
+      style: `.fooo { color: red }`
     }
   ];
 
   const rowList = [
     {
       name: 'hacucuao', // tmp name
-      template: `<div contenteditable>une kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren </div>`
+      template: `<div class="hacucuao" contenteditable>une kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri standardı sahte metinler olarak kullanılmıştır.Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır. 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren </div>`,
+      style: `.hacucuao { color: blue }`
     }
   ]
 
-  let leylim;
 
   const onSave = () => {
     const res = leylim.getRowData()
