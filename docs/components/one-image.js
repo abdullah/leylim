f8dd919a38096fb7fa55a4b01edd547e3a099058
Abdullah Mara <zull.mara00@gmail.com>
2017-07-18 14:56:23 +0300

    moving components to folder

---
+++ docs/components/one-image.js
@@ -0,0 +1,6 @@
+const oneImage = {
+  name: 'fooo', // tmp name
+  thumbnail: 'http://www.truebeck.com/wp-content/uploads/2016/12/work-item-placeholder-Copy-150x150.png',
+  template: `<div class="fooo" contenteditable><i>Lorem</i> ipsum dolor sit amet.</div>`,
+  style: `.fooo { color: red }`
+}
