5a4adb14c47581a7b43ecf2dcdcdbcf7e2b91721
Abdullah Mara <zull.mara00@gmail.com>
2017-07-19 23:06:27 +0300

    new editable logic
    for each editable area of row,
    initial and generate state

--- example/main.js
+++ example/main.js
@@ -12,7 +12,7 @@ window.onload = () => {
     {
       name: 'wiki-info', // tmp name
       thumbnail: 'http://www.truebeck.com/wp-content/uploads/2016/12/work-item-placeholder-Copy-150x150.png',
-      template: `<div class="wiki-info" contenteditable><h1>Leylim Ley </h1> Bestesi Zülfü Livaneli’ye aittir. Türkünün üç dörtlükten oluşan sözleri, Sabahattin Ali’nin Ses (1937) adlı öykü kitabına adını veren “Ses” öyküsünde yer alır. Öykünün kahramanı yol amelesi Sivaslı Ali, çadırının önünde durup saz çalıp, bu türküyü söyler. Zülfü Livaneli, öyküyü okurken bu türkünün sözlerinden çok etkilendiğini ve bestelediğini açıklamıştır.
+      template: `<div class="wiki-info" contenteditable="false"><h1>1Leylim Ley </h1> Bestesi Zülfü Livaneli’ye aittir. Türkünün üç dörtlükten oluşan sözleri, Sabahattin Ali’nin Ses (1937) adlı öykü kitabına adını veren “Ses” öyküsünde yer alır. Öykünün kahramanı yol amelesi Sivaslı Ali, çadırının önünde durup saz çalıp, bu türküyü söyler. Zülfü Livaneli, öyküyü okurken bu türkünün sözlerinden çok etkilendiğini ve bestelediğini açıklamıştır.
       <p>Eseri seslendirilen sanatçılar arasında İbrahim Tatlıses, Edip Akbayram, Zerrin Özer, Leman Sam, Zara, Özdemir Erdoğan yer alır.</p>
       </div>`,
       style: `.wiki-info { color: black }`
