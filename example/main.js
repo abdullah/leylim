21af4d0a26812c1782e072ca06d1f69e7ca50cef
Abdullah Mara <zull.mara00@gmail.com>
2017-07-31 12:38:10 +0300

    Update docs

--- example/main.js
+++ example/main.js
@@ -5,7 +5,24 @@ window.onload = () => {
   let components = lComponents;
 
   // Dummy row data
-  const rowList = []
+  const rowList = [{
+    name: 'l-one-column', // tmp name
+    thumbnail: 'l-one.png',
+    template: `
+    <div class="l-one-column">
+      <div class="l-col" contenteditable>
+        <h3>Title</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem impedit autem dolor explicabo dolorem adipisci, magnam ipsa sit nostrum nam ipsum dicta neque repellendus nihil cupiditate officiis, consequatur beatae?</p>
+      </div>
+      <div class="l-col" contenteditable>
+        <h3>Title</h3>
+        <div><img src="https://www.fandomsky.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/_/a_1_3.jpg" /></div>
+      </div>
+    </div>`,
+    style: `.l-one-column {
+      display: flex;
+    }`,
+  }]
 
   const onSave = () => {
     const res = leylim.getRowData();
@@ -16,6 +33,10 @@ window.onload = () => {
 
   Leylim.use(editImage, {
     test: 1
+  });
+
+  Leylim.use(modal, {
+    sizet: 'large'
   });
 
   leylim = new Leylim({
