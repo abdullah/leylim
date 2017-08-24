e9cc7783e0a0044dfe5a89dde45d4ee381060921
Abdullah Mara <zull.mara00@gmail.com>
2017-08-24 23:59:49 +0300

    update example row list

--- example/main.js
+++ example/main.js
@@ -5,40 +5,50 @@ window.onload = () => {
   let components = lComponents;
 
   // Dummy row data
-  const rowList = [{
-    name: 'l-one-column', // tmp name
-    thumbnail: 'l-one.png',
-    template: `
-    <div class="l-one-column">
-      <div class="l-col" contenteditable>
-        <h3>Title</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque rem impedit autem dolor explicabo dolorem adipisci, magnam ipsa sit nostrum nam ipsum dicta neque repellendus nihil cupiditate officiis, consequatur beatae?</p>
-      </div>
-      <div class="l-col" contenteditable>
-        <h3>Title</h3>
-        <div><img src="https://www.fandomsky.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/_/a_1_3.jpg" /></div>
-      </div>
-    </div>`,
-    style: `.l-one-column {
-      display: flex;
-    }`,
-  }]
+  const rowList = [
+    {
+      name: 'l-one-column', // tmp name
+      thumbnail: 'l-one.png',
+      template: `
+        <div class="l-one-column">
+          <div class="l-col" contenteditable>
+            <h3>Baratheon</h3>
+            <p>King Robert I Baratheon was the ruler of the Seven Kingdoms, formally styled as Robert of the House Baratheon, the First of His Name, King of the Andals and the First Men, Lord of the Seven Kingdoms and Protector of the Realm.</p>
+
+            <p>Robert, a great warrior and charismatic man in his youth, took the throne through conquest in the war known as Robert's Rebellion, which began when the Prince of Dragonstone, Rhaegar Targaryen, allegedly abducted Robert's betrothed, Lyanna Stark. Killing Rhaegar at the Battle of the Trident, his climactic duel turned the tides of the war in his favor. After the war and following Lyanna's death, Robert married Cersei Lannister to cement an alliance with House Lannister to hold the kingdoms together, but the marriage was loveless.</p>
+
+            <p>Robert also had two younger brothers, Stannis and Renly. He and Cersei have three children: Joffrey, Myrcella and Tommen. However, unbeknownst to Robert and most of the realm, all three were actually fathered by Cersei's twin brother, Jaime Lannister. On the other hand, Robert had many bastards from other women, including Gendry and Barra.</p>
+          </div>
+          <div class="l-col" contenteditable>
+            <img src="https://www.ablogofthrones.com/wp-content/uploads/2016/03/Robert-Baratheon-Kills-Rhaegar-Targaryen-FEAT.jpg"
+              />
+          </div>
+        </div>
+        `,
+      style: `
+        .l-one-column {
+            display: flex;
+            flex-flow: wrap;
+        }
+        .l-col {
+          width: 50%;
+        }
+        .l-col img {
+          max-width: 100%
+        }
+        `
+    }
+  ];
 
   const onSave = () => {
     const res = leylim.getRowData();
     for (var ii = 0; ii < res.length; ii++) {
       console.log(`${ii} ROW -> ${res[ii]}`);
     }
-  }
+  };
 
-  Leylim.use(modal, {
-    sizet: 'large'
-  });
-
-  Leylim.use(editImage, {
-    test: 1
-  });
-
+  Leylim.use(modal);
+  Leylim.use(editImage);
 
   leylim = new Leylim({
     el: '#app',
@@ -47,15 +57,14 @@ window.onload = () => {
     rowList,
     buttons: [
       {
-        text: "Save",
-        class: "save-class-example",
+        text: 'Save',
+        class: 'save-class-example',
         handler: onSave
       }
     ],
-    beforeRowUpdate(o){
+    beforeRowUpdate(o) {
       console.log(o.node);
-      return o
+      return o;
     }
   });
-}
-
+};
