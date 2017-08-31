07758db4d84168be6df0bef68801e1ef9198e585
Abdullah Mara <zull.mara00@gmail.com>
2017-08-31 15:10:13 +0300

    test: update bus

--- test/bus.test.js
+++ test/bus.test.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import Bus from '../lib/bus';
+import Leylim from '../lib/index';
 var assert = require('assert');
 var expect = require('expect.js');
 import { rowList } from './mocks/index';
@@ -19,5 +20,9 @@ describe('LEYLIM BUS', () => {
 
   it('should work Bus fire', () => {
     expect(Bus.fire).withArgs('ANY_EVENT', null, {}).to.not.throwException();
+  });
+
+  it('should work forceUpdate fire', () => {
+    expect(Leylim.forceUpdate).to.not.throwException();
   });
 });
